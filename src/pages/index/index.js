d0adf1fc1adea1be89245595d591d260130ee52e
cc <116012411@qq.com>
2018-06-01 16:30:36 +0800

    test layout

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -18,19 +18,43 @@ class Index extends Component {
   }
   render () {
     return (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="2">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className='logo' />
+          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+            <Menu.Item key='1' >
+              <Icon type='pie-chart' />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key='2'>
+              <Icon type='desktop' />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key='sub1'
+              title={<span><Icon type='user' /><span>User</span></span>}
+            >
+              <Menu.Item key='3'>Tom</Menu.Item>
+              <Menu.Item key='4'>Bill</Menu.Item>
+              <Menu.Item key='5'>Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key='sub2'
+              title={<span><Icon type='team' /><span>Team</span></span>}
+            >
+              <Menu.Item key='6'>Team 1</Menu.Item>
+              <Menu.Item key='8'>Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key='9'>
+              <Icon type='file' />
+              <span>File</span>
+            </Menu.Item>
           </Menu>
+        </Sider>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
@@ -48,6 +72,7 @@ class Index extends Component {
             Ant Design ©2016 Created by Ant UED
           </Footer>
         </Layout>
+      </Layout>
     )
   }
 }
