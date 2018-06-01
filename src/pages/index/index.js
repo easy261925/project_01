cc1ababe61be457e3af8b8fc7f7edf8322dbcc5a
cc <116012411@qq.com>
2018-06-01 16:27:46 +0800

    test layout

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -18,43 +18,6 @@ class Index extends Component {
   }
   render () {
     return (
-<<<<<<< HEAD
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className='logo' />
-          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-            <Menu.Item key='1' >
-              <Icon type='pie-chart' />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key='2'>
-              <Icon type='desktop' />
-              <span>Option 2</span>
-            </Menu.Item>
-            <SubMenu
-              key='sub1'
-              title={<span><Icon type='user' /><span>User</span></span>}
-            >
-              <Menu.Item key='3'>Tom</Menu.Item>
-              <Menu.Item key='4'>Bill</Menu.Item>
-              <Menu.Item key='5'>Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key='sub2'
-              title={<span><Icon type='team' /><span>Team</span></span>}
-            >
-              <Menu.Item key='6'>Team 1</Menu.Item>
-              <Menu.Item key='8'>Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key='9'>
-              <Icon type='file' />
-              <span>File</span>
-            </Menu.Item>
-=======
       <Layout className="layout">
         <Header>
           <div className="logo" />
@@ -67,9 +30,7 @@ class Index extends Component {
             <Menu.Item key="2">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
->>>>>>> dc81def7b28e4d48cf9a16ef7dd3675afaa0f6fb
           </Menu>
-        </Sider>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
