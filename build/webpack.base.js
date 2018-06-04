07761b921c4ff011d657f180dfad89cf7ab92fa2
cc <116012411@qq.com>
2018-06-04 16:16:40 +0800

    edit theme

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer');
+const theme = require('../theme');
 
 const resolve = (dir) => {
   return path.join(__dirname, '..', dir)
@@ -57,17 +59,6 @@ module.exports = {
         },
         exclude: /node_modules/
       }, {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }, {
-            loader: 'less-loader'
-          }
-        ]
-      }, {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
@@ -78,10 +69,83 @@ module.exports = {
             }
           }
         ]
-      }
+      },
+      // Parse less files and modify variables
+      {
+        test: /\.less$/,
+        use: [
+          require.resolve('style-loader'),
+          ({ resource }) => ({
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: /\.module\.less/.test(resource),
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          }),
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+          {
+            loader: require.resolve('less-loader'),
+            options: {
+              modifyVars: theme,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          ({ resource }) => ({
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: /\.module\.css/.test(resource),
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          }),
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      },
     ]
   },
-  plugins: [new HTMLWebpackPlugin({template: 'index.html', filename: 'index.html', inject: 'body'})],
+  plugins: [new HTMLWebpackPlugin({ template: 'index.html', filename: 'index.html', inject: 'body' })],
   resolve: {
     alias: {
       Component: path.join(__dirname, '..', 'src/components/')
